27d012565cd68e4f77212356d977e3a658f0516c
rashmikaofficial98@gmail.com <rashmikaofficial98@gmail.com>
2021-07-24 19:38:35 +0530

    include mongoose

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 
 const express= require("express");
 const bodyParser=require("body-parser"); 
-
+const mongoose = require("mongoose"); 
 const app= express();
 var item=["BMW", "FORD","Nissan"];
 
@@ -13,19 +13,48 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:  true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+
+const itemsSchema = {
+    name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema); 
+
+const item1 = new Item({
+    name: "Welcome to yur To-Do list!"
+});
+const item2 = new Item({
+    name: "Hit the + button to add a new item"
+});
+const item3 = new Item({
+    name: "<-- Hit this to delete an item"
+});
+const defaultItems =  [item1, item2, item3];
+
+Item.insertMany(defaultItems, function(err){
+    if(err){
+        console.log(err);
+    }else{
+        console.log("Successfully Insert...");
+    }
+});
+
+
+
 app.get("/", function(req, res){
 
-    var today= new Date();
+    // var today= new Date();
 
-    var options={
-        year:"numeric",
-        month:"long",
-        day:"numeric"
-    };
+    // var options={
+    //     year:"numeric",
+    //     month:"long",
+    //     day:"numeric"
+    // };
 
     var day =today.toLocaleDateString("en-US", options);
 
-    res.render("list", {kindDay: day, newListItems: item});
+    res.render("list", {kindDay: "Today", newListItems: item});
 
 });
 
